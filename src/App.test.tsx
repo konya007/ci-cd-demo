2a4f449db73ad8b38f1b4ea81707cf0ce32be472
Nguyen Thanh Trieu <trieukon1011@gmail.com>
2025-08-17 01:40:23 +0700

    demo 1

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,6 @@ import { describe, it, expect } from 'vitest';
 
 describe('Demo CI/CD', () => {
   it('should pass this demo test', () => {
-    expect(1 + 1).toBe(2);
+    expect(1 + 3).toBe(4);
   });
 });
